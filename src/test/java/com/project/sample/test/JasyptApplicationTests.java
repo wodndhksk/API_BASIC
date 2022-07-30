2e43776b01d8771b6395b01db0dc5daa6d8c3d08
Jaewoo Kim <wodndhksk@gmail.com>
2022-07-30 14:57:00 +0900

    DB 계정정보 암호화

---
+++ src/test/java/com/project/sample/test/JasyptApplicationTests.java
@@ -0,0 +1,29 @@
+package com.project.sample.test;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JasyptApplicationTests {
+
+    @Test
+    void jasypt() {
+        String url = "";
+        String username = "";
+        String password = "";
+
+        System.out.println("url 정보 암호화 값 :" + jasyptEncoding(url));
+        System.out.println("계정 암호화값 : " + jasyptEncoding(username));
+        System.out.println("비번 암호화 값 : " + jasyptEncoding(password));
+    }
+
+    public String jasyptEncoding(String value) {
+
+        String key = "my_jasypt_key";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword(key);
+        return pbeEnc.encrypt(value);
+    }
+}
